2e9921a911e4ec6ce30f6b679c9847141a2ecb29
Akashbaksi <a.a.stars.akash.baksi@gmail.com>
2022-09-13 22:28:41 +0530

    Fix palindrome number chek

---
+++ 1st project palindrome number .js
@@ -0,0 +1,61 @@
+  
+
+
+
+
+             
+
+  
+  
+
+
+
+
+
+     //------------************--------check-\/-palindrome------------***********------------//
+                              
+  					
+					let Enter_Number=787;
+
+                              
+       //----------******-------------------------\/---------------------******-----------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------------------
+let revnum=0;
+let check_palindrome=Enter_Number;
+while(Enter_Number>0){
+  num=Enter_Number%10;
+  num=Enter_Number%10;
+  revnum=revnum*10+num;
+  Enter_Number=Math.floor (Enter_Number/10);
+}if (check_palindrome==revnum){
+  console.log("Yes");
+}else{
+  console.log("No")
+}
--- practice.js
+++
@@ -1,5 +0,0 @@
-let x=10;
-
-let y=20;
-
-console.log(x*y);
