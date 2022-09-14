528e21ea1fa014dece9c43b8b724349387b87e5e
Akash <112754789+Akashbaksi@users.noreply.github.com>
2022-09-14 20:29:55 +0530

    Update prime.js

--- prime.js
+++ prime.js
@@ -1,6 +1,19 @@
-// Prime or not
-let number=21;
 
-for(let i=0;i>=21; i++){
+
+
+
+for (let i=1;i<=21;i++){
+  let count=0;
+  for (let j=1;j<=i;j++){
+    if(i%j==0){
+      count++;
+    }
+  }
+
+if(count==2){
+  console.log(i,"Prime Number");
     
+  }else {
+  console.log(i,"Not Prime Number" )
+  }
 }
